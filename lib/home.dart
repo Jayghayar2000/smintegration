6f5baef2fad40a79139840da7c23f94c099df02b
Jay Ghayar <58882580+Jayghayar2000@users.noreply.github.com>
2021-06-15 14:54:59 +0530

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _isLoggedIng = false;
+  late GoogleSignInAccount _userObjg;
+  GoogleSignIn _googleSignIn = GoogleSignIn();
+  bool _isLoggedInf = false;
+  bool _isLoggedIn = false;
+  Map _userObjf = {};
+  @override
+  Widget build(BuildContext context) {
+    // MaterialColor color;
+    return Scaffold(
+      appBar: AppBar(title: Text("SMINntegration")),
+      body: Container(
+        color: Colors.grey,
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                  child: _isLoggedIn
+                      ? Center()
+                      : Column(
+                          children: [
+                            Image.asset("assets/images/logo_small.png"),
+                            Text("The Sparks Foundation",
+                                style: const TextStyle(fontSize: 30)),
+                            Text("...inspiring, innovating, integrating",
+                                style: const TextStyle(fontSize: 20))
+                          ],
+                        )),
+              Container(
+                child: _isLoggedIng
+                    ? Center(
+                        child: Column(
+                          children: [
+                            Container(
+                                padding: EdgeInsets.fromLTRB(0, 40, 0, 5),
+                                child: Image.network(
+                                    _userObjg.photoUrl.toString())),
+                            Text(_userObjg.displayName.toString(),
+                                style: const TextStyle(fontSize: 25)),
+                            Text(_userObjg.email,
+                                style: const TextStyle(fontSize: 15)),
+                            TextButton(
+                                onPressed: () {
+                                  _googleSignIn.signOut().then((value) {
+                                    setState(() {
+                                      _isLoggedIng = false;
+                                      _isLoggedIn = false;
+                                    });
+                                  }).catchError((e) {});
+                                },
+                                child: Container(
+                                  child: Text(" Logout ",
+                                      style: const TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.white,
+                                          backgroundColor: Colors.blueGrey)),
+                                ))
+                          ],
+                        ),
+                      )
+                    : Center(
+                        child: _isLoggedInf
+                            ? Column()
+                            : Column(
+                                children: [
+                                  Container(
+                                    padding:
+                                        EdgeInsets.fromLTRB(100, 10, 100, 0),
+                                    child: ElevatedButton(
+                                      child: Text("Login with Google"),
+                                      onPressed: () {
+                                        _googleSignIn.signIn().then((userData) {
+                                          setState(() {
+                                            _isLoggedIng = true;
+                                            _isLoggedIn = true;
+                                            _userObjg = userData!;
+                                          });
+                                        }).catchError((e) {
+                                          print(e);
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                  Container(
+                                    child: Text(
+                                      "Or",
+                                      style: TextStyle(fontFamily: "monospace"),
+                                    ),
+                                  )
+                                ],
+                              ),
+                      ),
+              ),
+              Container(
+                child: _isLoggedInf
+                    ? Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.fromLTRB(0, 40, 0, 5),
+                            child: Image.network(
+                              _userObjf["picture"]["data"]["url"],
+                            ),
+                          ),
+                          Text(_userObjf["name"],
+                              style: const TextStyle(fontSize: 25)),
+                          Text(_userObjf["email"],
+                              style: const TextStyle(fontSize: 15)),
+                          TextButton(
+                              onPressed: () {
+                                FacebookAuth.instance.logOut().then((value) {
+                                  setState(() {
+                                    _isLoggedInf = false;
+                                    _isLoggedIn = false;
+                                    _userObjf = {};
+                                  });
+                                });
+                              },
+                              child: Text(" Logout ",
+                                  style: const TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.white,
+                                      backgroundColor: Colors.blueGrey)))
+                        ],
+                      )
+                    : Center(
+                        child: _isLoggedIng
+                            ? Column()
+                            : Column(
+                                children: [
+                                  Container(
+                                    child: ElevatedButton(
+                                      child: Text("Login with Facebook"),
+                                      onPressed: () async {
+                                        FacebookAuth.instance.login(
+                                            permissions: [
+                                              "public_profile",
+                                              "email"
+                                            ]).then((value) {
+                                          FacebookAuth.instance
+                                              .getUserData()
+                                              .then((userData) {
+                                            setState(() {
+                                              _isLoggedInf = true;
+                                              _isLoggedIn = true;
+                                              _userObjf = userData;
+                                            });
+                                          });
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                  Container()
+                                ],
+                              ),
+                      ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+// SHA1: 32:9F:CC:64:5F:28:F5:D3:B3:96:ED:B4:91:14:20:D2:98:BC:53:C3
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Social Media Integration',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: HomePage(),
+    );
+  }
+}
